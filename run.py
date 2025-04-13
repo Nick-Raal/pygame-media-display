cdaa7bc393d61a0c7c0fa2c95f09a3686327728e
Nick-Raal <105031104+Nick-Raal@users.noreply.github.com>
2025-04-13 15:50:36 -0700

    Update run.py

--- run.py
+++ run.py
@@ -75,7 +75,7 @@ def open(f):
     pygame_menu.events.EXIT
     if f.endswith('.mp4'):
         clock = pygame.time.Clock()
-        cap = cv2.VideoCapture(f.path)
+        cap = cv2.VideoCapture(f)
         playing, img = cap.read()
         shape = img.shape[1::-1]
         while playing:
