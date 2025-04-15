0085e3d7e5e16c17a4e0b6473a0ca418ba6e59cc
Nick-Raal <105031104+Nick-Raal@users.noreply.github.com>
2025-04-14 20:50:51 -0700

    Update run.py

--- run.py
+++ run.py
@@ -142,6 +142,7 @@ while running:
             elif menu._index < 0:
                 menu._index = len(menu.get_widgets())
             menu.get_scrollarea()._scrollbars[0].scroll_to_widget(menu.get_widgets()[menu._index], scroll_parent=True)
+            print(menu.get_scrollarea()._scrollbars[0].get_value())
             menu.draw(screen)
             if event.key == pygame.key.key_code('a'):
                 menu.get_selected_widget().apply()
