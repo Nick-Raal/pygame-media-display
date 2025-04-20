e6bab52bf0406b4b72a9a00b9b8b9352e22e5064
Nick-Raal <105031104+Nick-Raal@users.noreply.github.com>
2025-04-20 14:28:37 -0700

    Update run.py

--- run.py
+++ run.py
@@ -131,9 +131,9 @@ while running:
                 mainmenu.get_current()._index = 0
             elif mainmenu.get_current()._index < 0:
                 mainmenu.get_current()._index = len(mainmenu.get_current().get_widgets()) - 1
+            mainmenu.get_current().select_widget(mainmenu.get_current().get_widget( mainmenu.get_current()._index))
             if event.key == pygame.key.key_code('a'):
-                mainmenu.get_current().get_selected_widget().apply(mainmenu.get_current().get_widget( mainmenu.get_current()._index))
-            mainmenu.get_current().select_widget()
+                mainmenu.get_current().get_selected_widget().apply()
             mainmenu.get_current().get_scrollarea().scroll_to_rect(mainmenu.get_current().get_selected_widget().get_rect())
         if event.key in (pygame.key.key_code('b'), pygame.K_ESCAPE):
             running = False
