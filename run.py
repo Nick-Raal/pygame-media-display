29e986dc1eeeaad91088ba4e68288c9f47964c02
Nick-Raal <105031104+Nick-Raal@users.noreply.github.com>
2025-04-20 13:54:11 -0700

    Update run.py

--- run.py
+++ run.py
@@ -113,9 +113,6 @@ while running:
     if mainmenu.is_enabled():
         mainmenu.get_current().update(pygame.event.get())
         mainmenu.get_current().draw(screen)
-    if (mainmenu.get_current().get_selected_widget()):
-        arrow.draw(screen, mainmenu.get_current().get_selected_widget())
-    
     # screen.blit(pygame.image.frombuffer(img.tobytes(), shape, "BGR"), (0, 0))
     update_display()
     # clock.tick(60)
@@ -141,6 +138,9 @@ while running:
         if event.key in (pygame.key.key_code('b'), pygame.K_ESCAPE):
             running = False
             break
+    if (mainmenu.get_current().get_selected_widget()):
+        arrow.draw(screen, mainmenu.get_current().get_selected_widget())
+    
 
 screen.fill((0, 0, 0))
 update_display()
