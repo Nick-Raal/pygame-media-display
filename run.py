d74e9ae2a2b71268a499c527736ac2f8e519f7b3
Nick-Raal <105031104+Nick-Raal@users.noreply.github.com>
2025-04-21 16:45:45 -0700

    Update run.py

--- run.py
+++ run.py
@@ -160,6 +160,6 @@ while running:
             if event.key == (pygame.key.key_code('b')):
                 print('b')
                 mainmenu.get_current().close()
-                print(mainmenu.get_current())
+                print(mainmenu.get_current().get_title())
 
 _exit()
