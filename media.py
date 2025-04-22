14281a5ea843a2572a59f9bf9a79a8adbb09fb3b
Nick-Raal <105031104+Nick-Raal@users.noreply.github.com>
2025-04-22 13:02:36 -0700

    Update media.py

--- media.py
+++ media.py
@@ -1,22 +1,28 @@
 import pygame
 import cv2
+from pathlib import Path
 
 class Media:
     """
     Represents a base media file.
 
     Attributes:
+        title (str): The name of the media object
         file (str): The path to the media file.
     """
     
     def __init__ (self, file):
         self.file = file
+        self.title = Path(file).stem
         
-    def open():
+    def open(self):
         print("default open")
         
+    def get_title(self):
+        return self.title
+        
 class Video(Media):
-    def open():
+    def open(self):
         try:
             cap = cv2.VideoCapture(self.file)
             img = cap.read()
