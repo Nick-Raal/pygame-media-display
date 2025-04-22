b889f0c05feb37a3653cfe85b13e04fdb1442e8c
Nick-Raal <105031104+Nick-Raal@users.noreply.github.com>
2025-04-21 17:00:43 -0700

    Update run.py

--- run.py
+++ run.py
@@ -115,7 +115,7 @@ folder = pygame_menu.Menu('Memories', 320, 240,
     enabled=False, 
     theme=custom_theme,
     overflow=True)
-folder.set_onclose(pygame_menu.events.BACK)
+folder.set_onclose(pygame_menu.events.RESET)
 
 file_types = ('.mp4', '.png') 
 files = [f for f in os.listdir('.') if f.endswith(file_types)]
