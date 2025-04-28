3a95b47b3f7dba9602f479851b290a7fdbbdb7a3
Nick-Raal <105031104+Nick-Raal@users.noreply.github.com>
2025-04-28 13:30:39 -0700

    linux moment

--- themes.py
+++ themes.py
@@ -5,7 +5,7 @@ custom_theme.title_font = pygame_menu.font.FONT_FRANCHISE
 custom_theme.title_font_size= 60
 custom_theme.widget_background_inflate_to_selection=True
 bgimage = pygame_menu.baseimage.BaseImage(
-    image_path=("\graphics\\bg.svg"),
+    image_path=("./graphics/bg.svg"),
     drawing_mode=pygame_menu.baseimage.IMAGE_MODE_CENTER
 )
 custom_theme.background_color = bgimage
