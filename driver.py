5079262a341a12951c73a1e0fc5e63ba4f75a0b9
Nick-Raal <105031104+Nick-Raal@users.noreply.github.com>
2025-04-29 15:24:26 -0700

    hope

--- driver.py
+++ driver.py
@@ -8,6 +8,7 @@ import pygame
 import os
 import signal
 import sys
+import numpy as np
 from displayhatmini import DisplayHATMini
 
 class DisplayHatController:   
@@ -28,11 +29,11 @@ class DisplayHatController:
         # Grab the pygame screen as a bytes object
         pixelbytes = pygame.transform.rotate(self.screen, 180).convert(16, 0).get_buffer()
         # Lazy (slow) byteswap:
-        pixelbytes = bytearray(pixelbytes)
-        pixelbytes[0::2], pixelbytes[1::2] = pixelbytes[1::2], pixelbytes[0::2]
+        pixelbytes = np.array(pixelbytes)
+        pixelbytes.byteswap()
         # Bypass the ST7789 PIL image RGB888->RGB565 conversion
-        for i in range(0, len(pixelbytes), 16384):
-            self.display_hat.st7789.data(pixelbytes[i:i + 16384])
+        for i in range(0, len(pixelbytes), 4096):
+            self.display_hat.st7789.data(pixelbytes[i:i + 4096])
         
     # Plumbing to convert Display HAT Mini button presses into pygame events
     def button_callback(self, pin):
