c68df092507aa685a3049f3670df914a6fe48b7a
Nick-Raal <105031104+Nick-Raal@users.noreply.github.com>
2025-04-29 18:56:39 -0700

    Update memorymodule.py

--- memorymodule.py
+++ memorymodule.py
@@ -200,7 +200,7 @@ class MemoryModule:
             expanded_old = old_rect.inflate(20, 20)
             expanded_new = new_rect.inflate(20, 20)
             
-            return pygame.Rect.unionall[expanded_new, expanded_old]
+            return pygame.Rect.unionall([expanded_new, expanded_old])
         
     def need_to_draw(self):
         self.has_drawn = True
