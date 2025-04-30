24834798eee2251b7db1b4ee17efba0a376310ae
Nick-Raal <105031104+Nick-Raal@users.noreply.github.com>
2025-04-30 11:42:07 -0700

    no extra selection effect

--- themes.py
+++ themes.py
@@ -1,11 +1,13 @@
 import pygame_menu
+import pygame_menu.widgets
 
 custom_theme = pygame_menu.themes.THEME_SOLARIZED.copy()
 custom_theme.title_font = pygame_menu.font.FONT_FRANCHISE
 custom_theme.title_font_size= 60
 custom_theme.title_font_color=(255, 255, 255)
 custom_theme.title_bar_style=pygame_menu.widgets.MENUBAR_STYLE_NONE
-custom_theme.widget_background_inflate_to_selection=True
+custom_theme.widget_selection_color=pygame_menu.widgets.NoneSelection
+#custom_theme.widget_background_inflate_to_selection=True
 bgimage = pygame_menu.baseimage.BaseImage(
     image_path="./graphics/bg.jpg",
     drawing_mode=pygame_menu.baseimage.IMAGE_MODE_FILL
