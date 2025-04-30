4a3055cf68411cd2ef9215b7ac503bc7f870b441
Nick-Raal <105031104+Nick-Raal@users.noreply.github.com>
2025-04-29 18:24:50 -0700

    Update memorymodule.py

--- memorymodule.py
+++ memorymodule.py
@@ -186,7 +186,7 @@ class MemoryModule:
             self.mainmenu.get_current().draw(self.screen)
             pygame.draw.rect(self.screen, (255, 0, 0), self.select_rect)
             self.has_drawn=True
-            return pygame.Rect(0,0,320,240)
+            return [pygame.Rect(0,0,320,240),]
         else:
             pygame.draw.rect(self.screen, (255, 0, 0), self.select_rect)
             return dirty_select_rect
