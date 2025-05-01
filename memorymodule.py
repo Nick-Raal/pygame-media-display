90b358def5dbebdff9ee9fa12dc0f981a3f5f2f9
Nick-Raal <105031104+Nick-Raal@users.noreply.github.com>
2025-04-30 19:33:48 -0700

    Update memorymodule.py

--- memorymodule.py
+++ memorymodule.py
@@ -171,10 +171,10 @@ class MemoryModule:
                     
         self.mainmenu_buttons = [open_button, settings_button, quit_button]
         for b in self.mainmenu_buttons:
-            b.set_onselect(lambda but = b: self.select_rect.change_target(but.get_rect().center + (but.get_rect().width, 0)))
+            b.set_onselect(lambda but = b: self.select_rect.change_target(but.get_rect()))
             
         for b in folder_buttons:
-            b.set_onselect(lambda but = b: self.select_rect.change_target(but.get_rect().center  + (but.get_rect().width, 0)))
+            b.set_onselect(lambda but = b: self.select_rect.change_target(but.get_rect()))
         
         self.mainmenu.set_onupdate(self.select)
         self.folder.set_onupdate(self.select)
@@ -184,7 +184,7 @@ class MemoryModule:
         self.folder.set_onbeforeopen(self.need_to_draw)
         self.settings.set_onbeforeopen(onbeforeopen=self.need_to_draw)
         
-        self.select_rect = SelectRect(self.mainmenu_buttons[1].get_rect().left - 30, self.mainmenu_buttons[0].get_rect().top, 20, 50, self.mainmenu_buttons[0].get_rect().center + (self.mainmenu_buttons[0].get_rect().width, 0))
+        self.select_rect = SelectRect(self.mainmenu_buttons[1].get_rect().left - 30, self.mainmenu_buttons[0].get_rect().top, 20, 50, self.mainmenu_buttons[0].get_rect())
 
     def drawing_handler(self):
         new_rect, old_rect = self.select_rect.update()
@@ -313,14 +313,12 @@ class MemoryModule:
 class SelectRect(pygame.Rect):
     def __init__(self, x, y, width, height, target=None):
         super().__init__(x, y, width, height)
-        self.target = target
-        self.current_position = self.center
-        self.timer = 0
+        self.change_target(target)
         self.duration = 0.2
         self.old_rect = self.copy()
     
     def change_target(self, new_target):
-        self.target = new_target
+        self.target = (new_target.center + (new_target.width, 0))
         self.current_position = self.center
         self.timer = 0
     
