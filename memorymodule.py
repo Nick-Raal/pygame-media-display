b9978d6a00bddd96fb0707e22c33a6343eb3f949
Nick-Raal <105031104+Nick-Raal@users.noreply.github.com>
2025-04-30 22:20:36 -0700

    initial commit

--- memorymodule.py
+++ memorymodule.py
@@ -250,7 +250,9 @@ class MemoryModule:
                 widg = menu.get_widgets()[menu._index]
                 widg.select(update_menu=True)
                 if(menu.get_selected_widget()):
-                    menu.get_scrollarea().scroll_to_rect(menu.get_selected_widget().get_rect())               
+                    menu.get_scrollarea().scroll_to_rect(menu.get_selected_widget().get_rect())
+                    print(menu.get_scrollarea().get_translate())
+                    self.select_rect.move(menu.get_scrollarea().get_translate())               
                     
     def exit_handler(self, event_list):
         """
