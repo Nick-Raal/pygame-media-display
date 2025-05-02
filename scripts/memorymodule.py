9b228afaba6cec1c5d05e9d8eee5661d90e46169
Nick-Raal <105031104+Nick-Raal@users.noreply.github.com>
2025-05-02 00:07:29 -0700

    Update memorymodule.py

--- scripts/memorymodule.py
+++ scripts/memorymodule.py
@@ -236,6 +236,7 @@ class MemoryModule:
                 elif event.key == (pygame.key.key_code('b')):
                     menu.close()
                     #menu.enable()
+                    print(menu.get_current())
                     self.need_to_draw(target_menu=menu.get_current())
                     
                 elif event.key == pygame.key.key_code('a'):
