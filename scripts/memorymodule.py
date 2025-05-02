4d36643a030d9f7607523b307e87cfa0fd79f05c
Nick-Raal <105031104+Nick-Raal@users.noreply.github.com>
2025-05-01 23:56:58 -0700

    Update memorymodule.py

--- scripts/memorymodule.py
+++ scripts/memorymodule.py
@@ -235,7 +235,7 @@ class MemoryModule:
                     menu._index += 1
                 elif event.key == (pygame.key.key_code('b')):
                     menu.close()
-                    self.need_to_draw(target_menu=menu)
+                    menu.enable()
                     
                 elif event.key == pygame.key.key_code('a'):
                     menu.get_selected_widget().apply()
