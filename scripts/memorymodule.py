0a9e434db2e4f2aba8de0df254a27846d1800a2c
Nick-Raal <105031104+Nick-Raal@users.noreply.github.com>
2025-05-01 22:53:46 -0700

    Update memorymodule.py

--- scripts/memorymodule.py
+++ scripts/memorymodule.py
@@ -210,7 +210,7 @@ class MemoryModule:
             
     def need_to_draw(self, current_menu=None, target_menu=None):
         print("menu open ", current_menu)
-        self.select_rect.reset_position(current_menu)
+        self.select_rect.reset_position(target_menu)
         self.has_drawn = False
         
     def select(self, event_list, menu):
